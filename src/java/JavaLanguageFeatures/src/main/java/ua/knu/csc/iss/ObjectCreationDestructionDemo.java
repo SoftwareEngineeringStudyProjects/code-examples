8ca0043a0ea10d1fe569ea5ae058816dd7553622
Kostiantyn Zhereb <zhereb@gmail.com>
2024-10-24 11:32:14 +0300

    Another approach for detecting when object is garbage collected
    Some code by ChatGPT
    
    Use WeakReference that doesn't prevent GC. After object is marked for GC, all weak references are cleared (their get method will return null). We can check this.
    
    Notice that order of messages for finalizable and non-finalizable objects are different - this demonstrates different stages of GC

--- src/java/JavaLanguageFeatures/src/main/java/ua/knu/csc/iss/ObjectCreationDestructionDemo.java
+++ src/java/JavaLanguageFeatures/src/main/java/ua/knu/csc/iss/ObjectCreationDestructionDemo.java
@@ -1,21 +1,63 @@
 package ua.knu.csc.iss;
 
 
+import java.lang.ref.WeakReference;
+
 class MyObject {
+    private MyObjectFinalize objFinalize;
+    private MyObjectNoFinalize objNoFinalize;
+
+    public MyObject(String id) {
+        objFinalize = new MyObjectFinalize(id);
+        objNoFinalize = new MyObjectNoFinalize((id));
+    }
+
+
+    @Override
+    public String toString() {
+        return "MyObject{" +
+                "objFinalize=" + objFinalize.getId() +
+                ", objNoFinalize=" + objNoFinalize.getId() +
+                '}';
+    }
+
+    public WeakReference<MyObjectNoFinalize> getWeakRef() {
+        return new WeakReference<>(objNoFinalize);
+    }
+}
+
+class MyObjectFinalize {
     private String id;
 
     // Constructor
-    public MyObject(String id) {
+    public MyObjectFinalize(String id) {
         this.id = id;
-        System.out.println("MyObject " + id + " created!");
+        System.out.println("MyObjectFinalize " + id + " created!");
+    }
+
+    public String getId() {
+        return id;
     }
 
     // Finalizer (Note: Not recommended for general use)
     @SuppressWarnings({ "removal"}) // removal should be correct, but IntelliJ IDEA still shows error
     @Override
     protected void finalize() throws Throwable {
-        System.out.println("MyObject " + id + " destroyed!");
+        System.out.println("MyObjectFinalize " + id + " destroyed!");
         super.finalize();
+    }
+}
+
+class MyObjectNoFinalize {
+    private String id;
+
+    public MyObjectNoFinalize(String id) {
+        this.id = id;
+        System.out.println("MyObjectNoFinalize " + id + " created!");
+    }
+
+    public String getId() {
+        return id;
     }
 }
 
@@ -25,7 +67,9 @@ public class ObjectCreationDestructionDemo {
 
         // Create new objects with distinct identifiers
         MyObject obj1 = new MyObject("1");
+        WeakReference<MyObjectNoFinalize> ref1 = obj1.getWeakRef();
         MyObject obj2 = new MyObject("2");
+        WeakReference<MyObjectNoFinalize> ref2 = obj2.getWeakRef();
 
         // Set obj1 to null, making it eligible for garbage collection
         obj1 = null;
@@ -45,6 +89,8 @@ public class ObjectCreationDestructionDemo {
             }
         }
 
+        checkWeakRef(ref1, "1");
+
         // Set obj2 to null, making it also eligible for garbage collection
         obj2 = null;
         System.out.println("obj2 is now eligible for garbage collection.");
@@ -59,6 +105,18 @@ public class ObjectCreationDestructionDemo {
             e.printStackTrace();
         }
 
+        checkWeakRef(ref2, "2");
+
         System.out.println("End of main method.");
     }
+
+    private static void checkWeakRef(WeakReference<MyObjectNoFinalize> ref1, String id) {
+        if (ref1.get() == null) {
+            System.out.println("After GC: The object has been collected, id=" + id);
+        } else {
+            System.out.println("After GC: WeakReference.get() = " + ref1.get().getId());
+        }
+    }
+
+
 }
